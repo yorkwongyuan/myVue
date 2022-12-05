25fa3166908bfae6fe06262b873e38036b7a8901
yuan_wy_wang <631824375@qq.com>
2022-12-05 21:57:48 +0800

    first commit

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: './lib/Vue/index.js',
+      name: 'Counter',
+      fileName: 'counter'
+    }
+  }
+})
